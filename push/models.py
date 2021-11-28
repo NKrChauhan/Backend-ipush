86709688026428944ca43b9376babb4eea6d699e
NKrChauhan <nitish@pushowl.com>
2021-11-28 11:22:40 +0530

    update:max_length for endpoint

--- push/models.py
+++ push/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Subscriptions(models.Model):
-    endpoint = models.URLField(max_length=200, blank=False, null=False, unique=True)
+    endpoint = models.URLField(max_length=500, blank=False, null=False, unique=True)
     public_key = models.CharField(max_length=200, blank=False, null=False)
     auth_key = models.CharField(max_length=200, blank=False, null=False)
     subscribe_time = models.DateTimeField(auto_now_add=True)

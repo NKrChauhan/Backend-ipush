8313f62abf7ec68eb7bd0d656e5f72859d15d0bf
NKrChauhan <nitish@pushowl.com>
2021-12-07 18:18:45 +0530

    add:test for send_notification api
    cleanup and structuring the data in a better way

--- push/tests.py
+++ push/tests.py
@@ -4,25 +4,49 @@ from rest_framework.test import APITestCase
 import json
 
 
-class PushNotificationApiTest(APITestCase):
+class SubscriptionApiTest(APITestCase):
+    url = reverse('push:subscribe')
+
     def test_subscribe_client_api_with_valid_data(self):
-        url = reverse('push:subscribe')
         valid_subscriber_request_data = {"endpoint": "https://some.pushservice.com/something-unique2121000",
                                          "public_key": "BIPUL12DLfytvTajnryr2PRdAgXS3HGKiLqndGcJGabyhHheJYlNGCeXl1dn18gSJ1WAkAPIxr4gK0_dQds4yiI=",
                                          "auth_key": "FPssNDTKnInHVndSTdbKFw=="
                                          }
-        response_with_valid_data = self.client.post(path=url, data=valid_subscriber_request_data, format='json')
+        response_with_valid_data = self.client.post(path=self.url, data=valid_subscriber_request_data, format='json')
         response_with_valid_data_body = json.loads(response_with_valid_data.content)
         self.assertEqual(response_with_valid_data.status_code, status.HTTP_201_CREATED)
         self.assertNotEqual(response_with_valid_data_body['response'], 'Invalid Data')
 
     def test_subscribe_client_api_with_invalid_data(self):
-        url = reverse('push:subscribe')
         invalid_subscriber_request_data = {"endpoint": "some.pushservice.com/something-unique2121000",
                                            "public_key": "BIPUL12DLfytvTajnryr2PRdAgXS3HGKiLqndGcJGabyhHheJYlNGCeXl1dn18gSJ1WAkAPIxr4gK0_dQds4yiI=",
                                            "auth_key": "FPssNDTKnInHVndSTdbKFw=="
                                         }
-        response_with_invalid_data = self.client.post(path=url, data=invalid_subscriber_request_data,format='json')
+        response_with_invalid_data = self.client.post(path=self.url, data=invalid_subscriber_request_data,format='json')
+        response_with_invalid_data_body = json.loads(response_with_invalid_data.content)
+        self.assertEqual(response_with_invalid_data.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_with_invalid_data_body['response'], 'Invalid Data')
+
+
+class SendNotificationApiTest(APITestCase):
+    url = reverse('push:send_notification')
+
+    def test_send_notification_api_with_valid_notification_information(self):
+        valid_notification_request_data = {"title": "SALE SALE!",
+                                           "message": "OMG so good to have discounts",
+                                           "action_link": "https://www.google.com"
+                                           }
+        response_with_valid_data = self.client.post(path=self.url, data=valid_notification_request_data, format='json')
+        response_with_valid_data_body = json.loads(response_with_valid_data.content)
+        self.assertEqual(response_with_valid_data.status_code, status.HTTP_201_CREATED)
+        self.assertNotEqual(response_with_valid_data_body['response'], 'Invalid Data')
+
+    def test_send_notification_api_with_invalid_notification_information(self):
+        valid_notification_request_data = {"title": "SALE SALE!",
+                                           "message": "OMG so good to have discounts",
+                                           "action_link": "www.google.com"
+                                           }
+        response_with_invalid_data = self.client.post(path=self.url, data=valid_notification_request_data, format='json')
         response_with_invalid_data_body = json.loads(response_with_invalid_data.content)
         self.assertEqual(response_with_invalid_data.status_code, status.HTTP_200_OK)
         self.assertEqual(response_with_invalid_data_body['response'], 'Invalid Data')

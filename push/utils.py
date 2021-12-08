0e976ec6a6d2021e722d3f9970815006534b99ea
NKrChauhan <nitish@pushowl.com>
2021-12-08 15:02:21 +0530

    web_push implementation

---
+++ push/utils.py
@@ -0,0 +1,26 @@
+import json
+import base64
+from pywebpush import webpush, WebPushException
+from pushNotification.settings import PRIVATE_KEY
+
+
+def send_web_push(subscription_object, notification_data):
+    try:
+        webpush(
+            subscription_info={
+                "endpoint": subscription_object.endpoint,
+                "keys": {
+                    "p256dh": subscription_object.public_key,
+                    "auth": subscription_object.auth_key
+                }},
+            data=json.dumps(notification_data),
+            vapid_private_key=PRIVATE_KEY,
+            vapid_claims={
+                "sub": "mailto:nitish@pushowl.com",
+            }
+        )
+    except WebPushException as ex:
+        print(f"I'm sorry can't do that: {repr(ex)}")
+        if ex.response and ex.response.json():
+            extra = ex.response.json()
+            print(f"Remote service replied with a {extra.code}:{extra.errno}, {extra.message}")
--- push/views.py
+++ push/views.py
@@ -5,6 +5,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 from .serializers import SubscriptionSerializer, NotificationSerializer
 from .models import Subscription
+from .utils import send_web_push
 
 
 @api_view(['POST'])
@@ -25,5 +26,7 @@ def send_notification(request, *args, **kwargs):
     active_subscriptions = Subscription.objects.filter(is_active=True)
     if notification_serializer.is_valid():
         notification_serializer.save()
+        for active_subscription in active_subscriptions:
+            send_web_push(subscription_object=active_subscription, notification_data=notification_serializer.validated_data)
         return Response({"response": notification_serializer.data}, status=status.HTTP_201_CREATED)
     return Response({"response": "Invalid Data", "error": notification_serializer.errors}, status=status.HTTP_200_OK)
